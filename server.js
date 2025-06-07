8be17b019803b03792d6310e3d6c312ad7862f36
Tehila Aharonovich <146730488+TehilaTheStudent@users.noreply.github.com>
2025-06-07 22:35:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from backend new content!!!' });
+  res.json({ message: 'Hello from backend new jjkjkljkljkljcontent!!!' });
 });
 
 app.listen(port, () => {
